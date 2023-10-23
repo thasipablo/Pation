6c4c7330f3068f7a628ede1913efd6ef9eec2b80
thasipablo <thasipablo@gmail.com>
2023-10-23 17:36:46 +0100

    add logo to the nav bar

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,7 +1,17 @@
+"use client"
+
+import { cn } from "@/lib/utils";
+import useScrollTop from "@/hooks/use-scroll-top";
+import Logo from "./logo";
+
 const Navbar = () => {
-  return ( 
-    <div>Navbar</div>
+  const scrolled = useScrollTop();
+
+  return (
+    <div className={cn("z-50 bg-background fixed top-0 flex items-center w-full p-6", scrolled && "border-b shadow-sm")}>
+      <Logo />
+    </div>
   );
 }
- 
+
 export default Navbar;
--- hooks/use-scroll-top.tsx
+++ hooks/use-scroll-top.tsx
@@ -1,19 +1,23 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-const useScrollTop = ( threshold = 10 ) => {
-  const [ scrolled, setScrolled ] = useState(false)
+const useScrollTop = (threshold = 10) => {
+  const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      setScrolled(window.screenY > threshold ? true : false )
-    }
+      if (window.scrollY > threshold) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
 
-    window.addEventListener("scroll", handleScroll)
-    
-    return () => window.removeEventListener("scroll", handleScroll)
-  }, [threshold])
+    window.addEventListener("scroll", handleScroll);
 
-  return scrolled
-}
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [threshold]);
+
+  return scrolled;
+};
 
 export default useScrollTop;
