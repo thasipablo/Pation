10f884e8dd6424080d0336f93bc89fac261b2d45
thasipablo <thasipablo@gmail.com>
2023-10-23 14:34:15 +0100

    add header component

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,17 +1,11 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import Heading from "./_components/heading";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Notion clone</h1>
-      <p>
-        <Link href="/test">
-          <Button variant="default" size="sm">
-            Send the ping
-          </Button>
-        </Link>
-      </p>
-    </main>
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center pag-y-8 px-6 pb-10">
+        <Heading />
+      </div>
+    </div>
   );
 }
