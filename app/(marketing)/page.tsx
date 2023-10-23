35dce944578746f16ae9260e0aa074afb1952fad
thasipablo <thasipablo@gmail.com>
2023-10-23 14:53:05 +0100

    add heroes section

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,10 +1,12 @@
 import Heading from "./_components/heading";
+import Heroes from "./_components/heroes";
 
 export default function Home() {
   return (
     <div className="min-h-full flex flex-col">
       <div className="flex flex-col items-center justify-center md:justify-start text-center pag-y-8 px-6 pb-10">
         <Heading />
+        <Heroes />
       </div>
     </div>
   );
