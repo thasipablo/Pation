44e00e884817eca11bca4a98f45d6bae29a8b82b
thasipablo <thasipablo@gmail.com>
2023-10-24 16:46:22 +0100

    set up convex clerk auth client

---
+++ convex/auth.config.js
@@ -0,0 +1,10 @@
+const convexClerkAuth = {
+  providers: [
+    {
+      domain: "https://pet-stingray-45.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ]
+};
+
+export default convexClerkAuth
