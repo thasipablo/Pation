3f2b0133b8c2f3ee7590f13d797a0ebc091623a3
thasipablo <thasipablo@gmail.com>
2023-10-24 15:32:08 +0100

    change the dark color code

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   const scrolled = useScrollTop();
 
   return (
-    <div className={cn("z-50 bg-background fixed top-0 flex items-center w-full p-6", scrolled && "border-b shadow-sm")}>
+    <div className={cn("z-50 bg-background dark:bg-[#1F1F1F] fixed top-0 flex items-center w-full p-6", scrolled && "border-b shadow-sm")}>
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
         <ModeToggle />
--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "./_components/navbar";
 
 const MarketingLayout = ({ children } : { children: React.ReactNode }) => {
   return ( 
-    <div className="h-full">
+    <div className="h-full dark:bg-[#1F1F1F]">
       <Navbar />
       <div className="h-full pt-40">{ children }</div>
     </div>
