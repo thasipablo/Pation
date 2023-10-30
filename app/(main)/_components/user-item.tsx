5933e51b99f4bfc1bfbaf8f002eec76c11a733be
thasipablo <thasipablo@gmail.com>
2023-10-30 17:14:21 +0100

    display user data in the sidebar

--- app/(main)/_components/user-item.tsx
+++ app/(main)/_components/user-item.tsx
@@ -1,6 +1,36 @@
+"use client"
+
+import {
+  Avatar,
+  AvatarImage
+ } from "@/components/ui/avatar"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+ } from "@/components/ui/dropdown-menu"
+import { useUser } from "@clerk/clerk-react"
+
 const UserItem = () => {
+  const { user } = useUser()
   return ( 
-    <h1>User item</h1>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <div role="button" className="flex items-center text-sm p-3 w-full hover:bg-primary/5">
+          <div className="gap-x-2 flex items-center max-w-[150px]">
+            <Avatar className="h-5 w-5">
+              <AvatarImage src={user?.imageUrl} />
+            </Avatar>
+            <span>
+              { user?.fullName}&apos;s Pation
+            </span>
+          </div>
+        </div>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
   );
 }
  
