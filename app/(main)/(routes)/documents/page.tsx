bef32bcd6129028c589f66d657606f0346c1d27b
thasipablo <thasipablo@gmail.com>
2023-10-30 16:51:48 +0100

    add empty illustration and avatar component

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,6 +1,38 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { useUser } from "@clerk/clerk-react"
+import { PlusCircle } from "lucide-react"
+import Image from "next/image"
+
 const Documents = () => {
+  const { user } = useUser()
+
   return (
-    <div>Documents doc</div>
+    <div className="h-full flex flex-col items-center justify-center snap-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="empty"
+        className="hidden dark:block"
+      />
+
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Pation
+      </h2>
+      <Button className="mt-4">
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
   )
 }
 
